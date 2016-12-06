46e80537300fd65b5cf74e1f1ee250686ecc5777
Shawn Petros <arashcuzi@gmail.com>
2016-12-06 10:34:26 -0600

    broke a test

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,6 @@ export const sum = (x, y) => x + y;
 export const tax = (p, r) => p * r / 100;
 export const total = (p, r) => sum(p, tax(p, r));
 export const fv = (p, n, r) => {
-  const i = 1 + (r / 100);
-  return parseInt(p * Math.pow(i, n));
+  // const i = 1 + (r / 100);
+  // return parseInt(p * Math.pow(i, n));
 };
