e111cdf1f2a4e24dd05c9dc344f7037e12348392
Shawn Petros <arashcuzi@gmail.com>
2016-12-13 10:33:22 -0600

    removed the paragraph for the demo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ const App  = () => {
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>This is a new paragraph!</p>
         </div>
       );
     }
